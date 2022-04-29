1f4bc119afaf1b2492d74b4304b350c57b6a1cc7
Hongqiang Zhou <hongqiang.zhou@hotmail.com>
2022-04-29 13:27:48 -0400

    Update NailingPlanks.java

--- NailingPlanks.java
+++ NailingPlanks.java
@@ -1,24 +1,23 @@
-// Solution with O((N + M) * N) complexity, score 50%. Timeout exceptions in the test.
+// Solution with score 75% and a few timeout exceptions.
 class Solution {
     public int solution(int[] A, int[] B, int[] C) {
         // write your code in Java SE 8
-        Plunk[] plunks = new Plunk[A.length];
-        for (int i = 0; i < A.length; i++) {
-            plunks[i] = new Plunk(A[i], B[i]);
-        }
-        Arrays.sort(plunks, Comparator.comparing(e1 -> e1.x));
+        List<Plank> planks = IntStream.range(0, A.length).boxed()
+                .map(i -> new Plank(A[i], B[i]))
+                .sorted(Comparator.comparing(e -> e.x))
+                .collect(Collectors.toList());
 
         for (int i = 0; i < C.length; i++) {
-            while(plunks.length > 0) {
-                int pIndex = findPlunk(C[i], plunks);
+            while(planks.size() > 0) {
+                int pIndex = findPlunk(C[i], planks);
                 if (pIndex > -1) {
-                    plunks = removePlunk(plunks, pIndex);
+                    planks.remove(pIndex);
                 } else {
                     break;
                 }
             }
 
-            if (plunks.length == 0) {
+            if (planks.size() == 0) {
                 return i + 1;
             }
         }
@@ -26,15 +25,15 @@ class Solution {
         return -1;        
     }
 
-    private int findPlunk(int nail, Plunk[] plunks) {
+    private int findPlunk(int nail, List<Plank> planks) {
         int lower = 0;
-        int upper = plunks.length -1;
+        int upper = planks.size() -1;
 
         while (lower <= upper) {
             int mid = (lower + upper) / 2;
-            if (plunks[mid].x > nail) {
+            if (planks.get(mid).x > nail) {
                 upper = mid - 1;
-            } else if (plunks[mid].y < nail) {
+            } else if (planks.get(mid).y < nail) {
                 lower = mid + 1;
             } else {
                 return mid;
@@ -43,25 +42,11 @@ class Solution {
         return -1;
     }
 
-    private Plunk[] removePlunk(Plunk[] x, int index) {
-        Plunk[] res = new Plunk[x.length-1];
-
-        int j = 0;
-        for (int i = 0; i < x.length; i++) {
-            if (i != index) {
-                res[j] = x[i];
-                j++;
-            }
-        }
-
-        return res;
-    }
-
-    private class Plunk {
+    private class Plank {
         public int x;
         public int y;
 
-        Plunk(int x, int y) {
+        Plank(int x, int y) {
             this.x = x;
             this.y = y;
         }
