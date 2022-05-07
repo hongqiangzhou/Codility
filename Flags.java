63693112b5a86da6f78f4bedd38bab0760abe116
Hongqiang Zhou <hongqiang.zhou@hotmail.com>
2022-05-07 16:40:14 -0400

    Update Flags.java

--- Flags.java
+++ Flags.java
@@ -1,3 +1,4 @@
+// Below solution has 100% score.
 class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
@@ -35,5 +36,35 @@ class Solution {
         }
 
         return res;        
+    }
+}
+
+// Another solution, score: 80%, a few timeout exceptions
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        List<Integer> peaks = new ArrayList<Integer>();
+        for (int i = 1; i < A.length - 1; i++) {
+            if (A[i-1] < A[i] && A[i+1] < A[i]) peaks.add(i);
+        }
+
+        if (peaks.size() < 2) return peaks.size();
+
+        int ans = 1;
+        while(true) {
+            int numFlags = 1;
+            int prevPeak = peaks.get(0);
+            for (int i = 1; i < peaks.size(); i++) {
+                if (peaks.get(i) >= prevPeak + ans) {
+                    numFlags++;
+                    prevPeak = peaks.get(i);
+                    if (numFlags == ans) break;
+                }
+            }
+            if (numFlags < ans) break;
+            ans++;
+        }
+
+        return ans - 1;
     }
 }
