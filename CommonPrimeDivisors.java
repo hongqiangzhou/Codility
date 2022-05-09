cadbd66b4b50afd13c101f196b4ed86c2186c1c3
Hongqiang Zhou <hongqiang.zhou@hotmail.com>
2022-05-08 22:29:57 -0400

    Update CommonPrimeDivisors.java

--- CommonPrimeDivisors.java
+++ CommonPrimeDivisors.java
@@ -28,3 +28,50 @@ class Solution {
         return x;
     }
 }
+
+
+// Straightfoward solution, score: 53%, OutOfMemoryExceptions
+class Solution {
+    public int solution(int[] A, int[] B) {
+        // write your code in Java SE 8
+        int maxA = Arrays.stream(A).max().getAsInt();
+        int maxB = Arrays.stream(B).max().getAsInt();
+        int max = Math.max(maxA, maxB);
+        int sqrtN = (int) Math.ceil(Math.sqrt(max));
+        Boolean[] primeArray = new Boolean[1+max];
+        Arrays.fill(primeArray, true);
+        primeArray[0] = false;
+        primeArray[1] = false;
+
+        for (int i = 2; i < sqrtN; i++) {
+            if (primeArray[i]) {
+                for (int j = 2*i; j <= max; j = j+i) {
+                    primeArray[j] = false;
+                }
+            }
+        }
+
+        List<Integer> primesList = IntStream.rangeClosed(0, max).boxed()
+            .filter(i -> primeArray[i])
+            .collect(Collectors.toList());
+
+        int ans = 0;
+        for (int i = 0; i < A.length; i++) {
+           Set<Integer> setA = primeDivisors(A[i], primesList);
+           Set<Integer> setB = primeDivisors(B[i], primesList);
+           if (setA.containsAll(setB) && setB.containsAll(setA))
+               ans++;
+        }
+
+        return ans;
+    }
+
+    private Set<Integer> primeDivisors(int a, List<Integer> primesList) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < primesList.size() && primesList.get(i) <= a; i++) {
+           if (a % primesList.get(i) == 0) set.add(primesList.get(i));
+        }
+
+        return set;
+    }
+}
