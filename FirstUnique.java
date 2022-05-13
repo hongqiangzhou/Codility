4345a52753c35ca6b5c6ed2ce436ab31eac0e190
Hongqiang Zhou <hongqiang.zhou@hotmail.com>
2022-05-12 22:04:02 -0400

    Update FirstUnique.java

--- FirstUnique.java
+++ FirstUnique.java
@@ -14,3 +14,24 @@ class Solution {
     }
 }
 
+// Score: 90%, O(N*log(N))
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if (map.containsKey(A[i]))
+                map.put(A[i], -1);
+            else
+                map.put(A[i], i);
+        }
+
+        Optional<Map.Entry<Integer, Integer>> entry = map.entrySet().stream()
+                .filter(e -> e.getValue() != -1)
+                .max(Comparator.comparing(e -> -e.getValue()));
+        if (entry.isPresent())
+            return entry.get().getKey();
+        else
+            return -1;
+    }
+}
