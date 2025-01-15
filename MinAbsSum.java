32a43ca9a247d235d1e132cb71e9e5a851c7ca75
Hongqiang Zhou <hongqiang.zhou@hotmail.com>
2025-01-14 23:42:19 -0500

    Update MinAbsSum.java

--- MinAbsSum.java
+++ MinAbsSum.java
@@ -1,21 +1,5 @@
-// Don't know what's wrong. Below solution gets 27% with a lot of errors.
-class Solution {
-    public int solution(int[] A) {
-        // write your code in Java SE 8
-        if (A.length == 0) return 0;
-
-        int dp[] = new int[A.length];
-        dp[0] = Math.abs(A[0]);
-        for (int i = 1; i < A.length; i++) {
-            dp[i] = Math.min(Math.abs(dp[i-1] - A[i]), Math.abs(dp[i-1] + A[i]));
-        }
-
-        return dp[A.length-1];        
-    }
-}
-
 // This solution follows `https://codility.com/media/train/solution-min-abs-sum.pdf`
-// Score: 63%, complexity: O(N**2*max(abs(A))), a lot of timeout exceptions.
+// Simple solution. Score: 63%, complexity: O(N**2*max(abs(A))), a lot of timeout exceptions.
 class Solution {
     public int solution(int[] A) {
         // Implement your solution here
@@ -42,6 +26,55 @@ class Solution {
             }
         }
 
+
+        return result;
+    }
+}
+
+// Improved solution. Score: 100%.
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A) {
+        // Implement your solution here
+        if (A.length == 0) return 0;
+        if (A.length == 1) return A[0];
+        
+        int maxA = 0;
+        int sumA = 0;
+        for (int i = 0; i < A.length; i++) {
+            A[i] = Math.abs(A[i]);
+            maxA = Math.max(maxA, A[i]);
+            sumA += A[i];
+        }
+
+        int[] count = new int[maxA + 1];
+        for (int a: A) {
+            count[a] += 1;
+        }
+
+        int[] dp = new int[sumA + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int i = 0; i <= maxA; i++) {
+            if (count[i] > 0) {
+                for (int val = 0; val <= sumA; val++) {
+                    if(dp[val] >= 0) {
+                        dp[val] = count[i];
+                    } else if (val >= i && dp[val-i] > 0) {
+                        dp[val] = dp[val-i] - 1;
+                    }
+                }
+            }
+        }
+
+        int result = sumA;
+        for (int i = 0; i <= sumA / 2; i++) {
+            if (dp[i] >= 0) {
+                result = Math.min(result, sumA - 2 * i);
+            }
+        }
 
         return result;
     }
